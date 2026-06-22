79850484e3b5e7c6297e836095b4699f95dc7ba1
richachandel01 <chandelricha24@gmail.com>
2026-06-22 14:21:48 +0530

    Day 7: Added database user authentication

--- backend/src/main/java/com/richa/aimockinterview/controller/AuthController.java
+++ backend/src/main/java/com/richa/aimockinterview/controller/AuthController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.richa.aimockinterview.dto.LoginRequestDto;
 import com.richa.aimockinterview.dto.LoginResponseDto;
+import com.richa.aimockinterview.entity.User;
+import com.richa.aimockinterview.repository.UserRepository;
 import com.richa.aimockinterview.security.jwt.JwtService;
 
 import lombok.RequiredArgsConstructor;
@@ -17,17 +19,30 @@ import lombok.RequiredArgsConstructor;
 public class AuthController {
 
     private final JwtService jwtService;
+    private final UserRepository userRepository;
 
     @PostMapping("/login")
     public LoginResponseDto login(
             @RequestBody LoginRequestDto request) {
 
+        User user = userRepository
+                .findByEmail(request.getEmail())
+                .orElseThrow(() ->
+                        new RuntimeException("User not found"));
+
+        if (!user.getPassword()
+                .equals(request.getPassword())) {
+
+            throw new RuntimeException("Invalid password");
+        }
+
         String token =
                 jwtService.generateToken(
-                        request.getEmail());
+                        user.getEmail());
 
         return LoginResponseDto.builder()
                 .token(token)
                 .build();
     }
 }
+
