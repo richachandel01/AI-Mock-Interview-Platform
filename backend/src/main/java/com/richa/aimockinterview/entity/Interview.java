29edd2794dc732d87f13eb22cda4d11dd67adcdd
richachandel01 <chandelricha24@gmail.com>
2026-06-24 00:18:58 +0530

    Day 9: Added Interview entity

---
+++ backend/src/main/java/com/richa/aimockinterview/entity/Interview.java
@@ -0,0 +1,46 @@
+package com.richa.aimockinterview.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "interviews")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Interview {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String role;
+
+    private String technology;
+
+    private String difficulty;
+
+    private String status;
+
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
