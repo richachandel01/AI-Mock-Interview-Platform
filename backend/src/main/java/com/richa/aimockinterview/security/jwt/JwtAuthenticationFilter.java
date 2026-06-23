78c3802130bce15c51d53b9728b6726eef2f745d
richachandel01 <chandelricha24@gmail.com>
2026-06-24 00:05:48 +0530

    Day 8: Integrated JWT with Spring Security context

--- backend/src/main/java/com/richa/aimockinterview/security/jwt/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/richa/aimockinterview/security/jwt/JwtAuthenticationFilter.java
@@ -3,8 +3,14 @@ package com.richa.aimockinterview.security.jwt;
 import java.io.IOException;
 
 import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.richa.aimockinterview.security.CustomUserDetailsService;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -15,9 +21,14 @@ import jakarta.servlet.http.HttpServletResponse;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
+    private final CustomUserDetailsService userDetailsService;
 
-    public JwtAuthenticationFilter(JwtService jwtService) {
+    public JwtAuthenticationFilter(
+            JwtService jwtService,
+            CustomUserDetailsService userDetailsService) {
+
         this.jwtService = jwtService;
+        this.userDetailsService = userDetailsService;
     }
 
     @Override
@@ -40,7 +51,29 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         String token = authHeader.substring(7);
 
         try {
-jwtService.extractEmail(token);        } catch (Exception e) {
+
+            String email =
+                    jwtService.extractEmail(token);
+
+            UserDetails userDetails =
+                    userDetailsService
+                            .loadUserByUsername(email);
+
+            UsernamePasswordAuthenticationToken authToken =
+                    new UsernamePasswordAuthenticationToken(
+                            userDetails,
+                            null,
+                            userDetails.getAuthorities());
+
+            authToken.setDetails(
+                    new WebAuthenticationDetailsSource()
+                            .buildDetails(request));
+
+            SecurityContextHolder.getContext()
+                    .setAuthentication(authToken);
+
+        } catch (Exception e) {
+
             response.setStatus(
                     HttpServletResponse.SC_UNAUTHORIZED);
             return;
