eef275d153d2fa123594318f86e0c4a7efe46e04
richachandel01 <chandelricha24@gmail.com>
2026-06-26 00:54:32 +0530

    Day 13: Added EvaluationResult entity

---
+++ backend/src/main/java/com/richa/aimockinterview/entity/EvaluationResult.java
@@ -0,0 +1,38 @@
+package com.richa.aimockinterview.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "evaluation_results")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class EvaluationResult {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer score;
+
+    @Column(length = 1000)
+    private String feedback;
+
+    @OneToOne
+    @JoinColumn(name = "answer_id")
+    private Answer answer;
+}
