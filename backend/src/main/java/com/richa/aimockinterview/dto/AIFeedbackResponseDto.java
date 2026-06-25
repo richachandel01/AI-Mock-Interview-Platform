a8be67dfea76081dc02acb4879baf77534f88646
richachandel01 <chandelricha24@gmail.com>
2026-06-26 01:36:45 +0530

    Day 15: Added AI feedback DTOs

---
+++ backend/src/main/java/com/richa/aimockinterview/dto/AIFeedbackResponseDto.java
@@ -0,0 +1,19 @@
+package com.richa.aimockinterview.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AIFeedbackResponseDto {
+
+    private Integer score;
+
+    private String feedback;
+}
