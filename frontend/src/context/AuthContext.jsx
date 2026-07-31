a5a08a9925508790f295904514e22249f7da11fa
richachandel01 <chandelricha24@gmail.com>
2026-07-31 12:35:38 +0530

    Day 25: Added authentication context

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,32 @@
+import { createContext, useContext, useState } from "react";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+
+    const [user, setUser] = useState(null);
+
+    const loginUser = (userData) => {
+        setUser(userData);
+        localStorage.setItem("user", JSON.stringify(userData));
+    };
+
+    const logoutUser = () => {
+        setUser(null);
+        localStorage.removeItem("user");
+    };
+
+    return (
+        <AuthContext.Provider
+            value={{
+                user,
+                loginUser,
+                logoutUser,
+            }}
+        >
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export const useAuth = () => useContext(AuthContext);
