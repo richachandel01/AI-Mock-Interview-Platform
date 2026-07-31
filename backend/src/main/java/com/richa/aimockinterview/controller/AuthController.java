ce8f1174e5d40f45052dc59f093ef41d028f05f4
richachandel01 <chandelricha24@gmail.com>
2026-07-31 12:52:52 +0530

    Day 26: Refactored authentication controller

--- backend/src/main/java/com/richa/aimockinterview/controller/AuthController.java
+++ backend/src/main/java/com/richa/aimockinterview/controller/AuthController.java
@@ -1,15 +1,12 @@
 package com.richa.aimockinterview.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.richa.aimockinterview.dto.LoginRequestDto;
 import com.richa.aimockinterview.dto.LoginResponseDto;
-import com.richa.aimockinterview.entity.User;
-import com.richa.aimockinterview.repository.UserRepository;
-import com.richa.aimockinterview.security.jwt.JwtService;
+import com.richa.aimockinterview.dto.RegisterRequestDto;
+import com.richa.aimockinterview.dto.RegisterResponseDto;
+import com.richa.aimockinterview.service.AuthService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,31 +15,19 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AuthController {
 
-    private final JwtService jwtService;
-    private final UserRepository userRepository;
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public RegisterResponseDto register(
+            @RequestBody RegisterRequestDto request) {
+
+        return authService.register(request);
+    }
 
     @PostMapping("/login")
     public LoginResponseDto login(
             @RequestBody LoginRequestDto request) {
 
-        User user = userRepository
-                .findByEmail(request.getEmail())
-                .orElseThrow(() ->
-                        new RuntimeException("User not found"));
-
-        if (!user.getPassword()
-                .equals(request.getPassword())) {
-
-            throw new RuntimeException("Invalid password");
-        }
-
-        String token =
-                jwtService.generateToken(
-                        user.getEmail());
-
-        return LoginResponseDto.builder()
-                .token(token)
-                .build();
+        return authService.login(request);
     }
 }
-
