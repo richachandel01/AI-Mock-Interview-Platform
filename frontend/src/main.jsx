f90ddd70f03fd1c110ab7977b51fc81b4e7a7921
richachandel01 <chandelricha24@gmail.com>
2026-07-31 12:59:41 +0530

    Day 26: Wrapped app with authentication context

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,9 @@ import "./index.css";
 import { AuthProvider } from "./context/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <AuthProvider>
-        <App />
-    </AuthProvider>
+    <React.StrictMode>
+        <AuthProvider>
+            <App />
+        </AuthProvider>
+    </React.StrictMode>
 );
