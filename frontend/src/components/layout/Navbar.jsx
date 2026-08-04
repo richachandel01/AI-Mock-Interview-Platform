e7ce829305358c977c00a4bc910e4c87f31751fd
richachandel01 <chandelricha24@gmail.com>
2026-08-04 15:10:15 +0530

    Day 27: Added logout functionality

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -1,17 +1,70 @@
-function Navbar() {
-  return (
-    <nav className="bg-slate-900 text-white px-8 py-4 flex justify-between items-center">
-      <h1 className="text-2xl font-bold">
-        AI Mock Interview
-      </h1>
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
 
-      <div className="flex gap-6">
-        <a href="/">Home</a>
-        <a href="/login">Login</a>
-        <a href="/signup">Signup</a>
-      </div>
-    </nav>
-  );
+function Navbar() {
+
+    const { authenticated, logout } = useAuth();
+
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+
+        logout();
+
+        navigate("/login");
+    };
+
+    return (
+
+        <nav className="bg-slate-900 text-white px-8 py-4 flex justify-between">
+
+            <h1 className="text-2xl font-bold">
+                AI Mock Interview
+            </h1>
+
+            <div className="flex gap-6">
+
+                <Link to="/">Home</Link>
+
+                {
+
+                    authenticated ? (
+
+                        <>
+
+                            <Link to="/dashboard">
+                                Dashboard
+                            </Link>
+
+                            <button onClick={handleLogout}>
+                                Logout
+                            </button>
+
+                        </>
+
+                    ) : (
+
+                        <>
+
+                            <Link to="/login">
+                                Login
+                            </Link>
+
+                            <Link to="/signup">
+                                Signup
+                            </Link>
+
+                        </>
+
+                    )
+
+                }
+
+            </div>
+
+        </nav>
+
+    );
 }
 
 export default Navbar;
