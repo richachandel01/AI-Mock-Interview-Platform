4a0593b00a1f809a5c962ae05d1c3fa56ead02b1
richachandel01 <chandelricha24@gmail.com>
2026-08-05 15:18:48 +0530

    Day 29: Built interview screen layout

--- frontend/src/pages/Interview/Interview.jsx
+++ frontend/src/pages/Interview/Interview.jsx
@@ -1,5 +1,32 @@
+import InterviewHeader from "./InterviewHeader";
+import QuestionCard from "./QuestionCard";
+import AnswerBox from "./AnswerBox";
+import ProgressBar from "./ProgressBar";
+import Timer from "./Timer";
+import InterviewFooter from "./InterviewFooter";
+
 function Interview() {
-  return <h1>Interview Page</h1>;
+    return (
+        <div className="min-h-screen bg-gray-100">
+
+            <InterviewHeader />
+
+            <div className="max-w-5xl mx-auto py-8 px-6">
+
+                <ProgressBar />
+
+                <QuestionCard />
+
+                <AnswerBox />
+
+                <InterviewFooter />
+
+            </div>
+
+            <Timer />
+
+        </div>
+    );
 }
 
 export default Interview;
