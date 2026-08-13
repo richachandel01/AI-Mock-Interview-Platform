2b80d5a12a06e779985de30e4c8b7bbbe4b80160
richachandel01 <chandelricha24@gmail.com>
2026-08-13 11:59:44 +0530

    feat(day34): make interview progress dynamic

--- frontend/src/pages/Interview/ProgressBar.jsx
+++ frontend/src/pages/Interview/ProgressBar.jsx
@@ -1,30 +1,36 @@
-function ProgressBar() {
+function ProgressBar({ currentQuestion, totalQuestions }) {
+
+    const percentage =
+        totalQuestions === 0
+            ? 0
+            : Math.round((currentQuestion / totalQuestions) * 100);
 
     return (
-
         <div className="mt-6">
 
             <div className="flex justify-between mb-2">
 
-                <span>Question 1 / 10</span>
+                <span>
+                    Question {currentQuestion} / {totalQuestions}
+                </span>
 
-                <span>10%</span>
+                <span>
+                    {percentage}%
+                </span>
 
             </div>
 
             <div className="w-full bg-gray-300 rounded-full h-3">
 
                 <div
-                    className="bg-blue-600 h-3 rounded-full"
-                    style={{ width: "10%" }}
+                    className="bg-blue-600 h-3 rounded-full transition-all"
+                    style={{ width: `${percentage}%` }}
                 />
 
             </div>
 
         </div>
-
     );
-
 }
 
 export default ProgressBar;
