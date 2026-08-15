789f607b69aca969b0095c49b91370184473d108
richachandel01 <chandelricha24@gmail.com>
2026-08-15 11:30:24 +0530

    feat(day36): finalize answer submission service

--- frontend/src/services/interviewService.js
+++ frontend/src/services/interviewService.js
@@ -1,21 +1,3 @@
-import api from "./api";
-
-export const getQuestionsByInterview = async (interviewId) => {
-
-    const response = await api.get(
-        `/questions/interview/${interviewId}`
-    );
-
-    return response.data;
-};
-
-export const getAllQuestions = async () => {
-
-    const response = await api.get("/questions");
-
-    return response.data;
-};
-
 export const submitAnswer = async (answerData) => {
 
     const response = await api.post(
